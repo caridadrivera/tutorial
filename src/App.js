f847884f5591e88e6b096f805d5e55ebbf88d444
Caridad Rivera <ccrive10@gmail.com>
2020-09-22 22:04:33 -0400

    full auto component setup

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
 import React from 'react';
 import './App.css';
-import AutoButton from './Components/autoBtn'
-import FireButton from './Components/fireBtn'
-import LifeButton from './Components/lifeBtn'
+import { useHistory } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import AutoButton from './Components/autoBtn';
+import FireButton from './Components/fireBtn';
+import LifeButton from './Components/lifeBtn';
+import mainAutoPage from './Components/mainAutoPage'
 
 export default function App() {
+  const history = useHistory();
 
   const handleAutoClick = () =>{
-    console.log('im here')
+      
   }
 
   const handleFireClick = () =>{
-    console.log('im here')
+    console.log('im here');
   }
 
   const handleLifeClick = () =>{
-    console.log('im here')
+    console.log('im here');
   }
   
   return (
-    <div className="App">
-     <AutoButton handleClick={handleAutoClick}/>
-     <FireButton handleFireClick={handleFireClick} />
-     <LifeButton handleLifeClick={handleLifeClick}/>
-    </div>
+    
+      <div className="App">
+        <AutoButton handleClick={handleAutoClick}/>
+        <FireButton handleFireClick={handleFireClick} />
+        <LifeButton handleLifeClick={handleLifeClick}/>
+      </div>
+    
+    
   );
 }
--- src/Components/autoBtn.js
+++ src/Components/autoBtn.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import mainAutoPage from './Components/mainAutoPage'
+
+import {useHistory, Router, Route, Link} from 'react-router-dom';
 
 export default function autoButton(props){
 
     console.log(props)
     return (
-        <button onClick={props.handleClick}>
-            AUTO
+        <main>
+<Router>
+         <button onClick={props.handleClick}>
+            <Link to="/auto">
+                    AUTO
+            </Link>
         </button>
+     </Router>
+        
+     <Route path="/auto" exact component={mainAutoPage}></Route>
+
+        </main>
+     
     )
 }
