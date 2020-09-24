ea5ef663343716dde74b8a91cafee05aa1973979
Caridad Rivera <ccrive10@gmail.com>
2020-09-24 18:27:21 -0400

    auto route works

--- src/Components/autoBtn.js
+++ src/Components/autoBtn.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function autoButton(props){
 
     console.log(props)
     return (
+        <Link to='/auto'>
         <button onClick={props.handleClick}>
             AUTO
         </button>
+        </Link>
+        
     )
 }
