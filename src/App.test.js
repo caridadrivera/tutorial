4b57fe94fcdbb89b822dd231f6296d68f5b57e87
Caridad Rivera <ccrive10@gmail.com>
2020-10-16 16:50:02 -0400

    refactoring

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
-import MainPage from '/components/mainPage';
+import MainPage from './components/mainPage';
+import renderer from 'react-test-renderer';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// test('renders learn react link', () => {
+//   const { getByText } = render(<App />);
+//   const linkElement = getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
 
 
+it('renders correctly', () =>{
+  const tree = renderer
+  .create(<MainPage/>)
+  .toJSON();
+  expect(tree).toMatchSnapShot()
+})
--- src/Components/mainPage.js
+++ src/Components/mainPage.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { withRouter } from 'react-router-dom'
-import AutoButton from './autoBtn';
-import FireButton from './fireBtn';
-import LifeButton from './lifeBtn';
+import { withRouter, Link, Router, Route } from 'react-router-dom'
+// import AutoButton from './autoBtn';
+// import FireButton from './fireBtn';
+// import LifeButton from './lifeBtn';
 
 
 
@@ -14,13 +14,17 @@ export default function mainPage(props){
     return (
 
           <div className="mainButtonContainer">
-                <AutoButton />
+            <div className="autoButton">
+                 <Link to="/auto">AUTO</Link>
+            </div>
+            <div className="fireButton">
+                <Link to="/fire">FIRE</Link>
+            </div>
+            <div className="lifeButton">
+                <Link to="/life">LIFE</Link>
+            </div>
             
-                 <FireButton />
-            
-               <LifeButton />
-
-        </div>
+         </div>
       
         
      
