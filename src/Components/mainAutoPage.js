92fad62c49541bd9d6fc19b4a1201ea287afe442
Caridad Rivera <ccrive10@gmail.com>
2020-10-29 20:09:49 -0400

    refactoring

--- src/Components/mainAutoPage.js
+++ src/Components/mainAutoPage.js
@@ -1,45 +1,47 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 
-function getAutoQuote(){
-    const quotingButton = document.querySelector('.autoQuoteBtn')
-    quotingButton.addEventListener('click', (e) => {
-    e.preventDefault();
-    console.log('I have been clicked.')
-   })
-
-}
 
 
-export default function mainAutoPage(){
-   
-    const autoQuote = 'Quoting'
-    const autoBilling = 'Billing';
-    const autoAccountMngmt = 'Account Management';
-   
-       
-    return (
 
-        
-        
-        <div className="autoPage">
-            
-            <button onClick={getAutoQuote} className="autoQuoteBtn">
-               {autoQuote}
-               
-            </button>
-
-            <button className="autoBillingBtn">
-               {autoBilling}
-            </button>
-
-            <button className="autoAccountBtn">
-                {autoAccountMngmt}
-            </button>
-        </div>
-    )
-
+export default class MainAutoPage extends Component {
 
     
+     getAutoQuote = () =>{
+        const quotingButton = document.querySelector('.autoQuoteBtn')
+        quotingButton.addEventListener('click', (e) => {
+        e.preventDefault();
+        console.log('I have been clicked.')
+       })
+    };
+      
+    render() {
+        const autoQuote = 'Quoting';
+        const autoBilling = 'Billing';
+        const autoAccountMngmt = 'Account Management';
+
+
+
+       
+       
+        return (
+    
+            <div className="autoPage">  
+                <button onClick={this.getAutoQuote} className="autoQuoteBtn">
+                {autoQuote}    
+                </button>
+
+                <button className="autoBillingBtn">
+                {autoBilling}
+                </button>
+
+                <button className="autoAccountBtn">
+                    {autoAccountMngmt}
+                </button>
+            </div>
+                )
+
+    }
+ 
 }
 
